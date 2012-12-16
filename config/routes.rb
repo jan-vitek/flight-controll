f5e15b5f563f716396a6a7018419ab7291045a6c
Jan Vitek <vitek@server.(none)>
2012-12-16 22:16:42 +0100

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -58,7 +58,7 @@ Pilotaz::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'pilots#index'
 
   # See how all your routes lay out with "rake routes"
 
