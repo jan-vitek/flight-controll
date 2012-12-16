f0c952f5f20f1626ff3125560cade80161cad59f
Jan Vitek <vitek@server.(none)>
2012-12-16 21:27:09 +0100

    removed bad migration files

--- db/migrate/20121216192044_add_fieldname_to_tablename2.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldnameToTablename2 < ActiveRecord::Migration
-  def change
-    change_column :pilots, :end, :datetime
-  end
-end
--- db/migrate/20121216194945_add_fieldname_to_tablename3.rb
+++
@@ -1,5 +0,0 @@
-class AddFieldnameToTablename3 < ActiveRecord::Migration
-  def change
-    add_column :tablename3s, :fieldname, :string
-  end
-end
